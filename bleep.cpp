1e8c45822166e42ca09afd52b007f357f28fe9a3
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2023-04-24 09:46:51 -0400

    Create bleep.cpp
    Creates sample items to run through Bleep function and outputs them after running the function.

---
+++ bleep.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "functions.hpp"
+
+int main(){
+
+//banned word and sample text it is being 
+std::string brocc = "broccoli";
+std::string sample = "This broccoli is amazing, dude. We need to go get some more broccoli. There is for sure nothing better than some well-cooked broccoli in this world. Especially in some cheese. It's the best, that broccoli. Did I make a \"b\" detector that's broken? Let's find out while I snack on some broccoli.";
+
+// pass bleep function on the text using the sample banned word, broccoli.
+bleep(brocc, sample);
+
+
+// output the text after bleeping.
+std::cout << sample;
+
+}
